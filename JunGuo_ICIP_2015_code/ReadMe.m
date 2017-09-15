103751a2cd0bcefed860943c3366f9e8483efed2
eeGuoJun <eeguojun@foxmail.com>
2017-09-15 17:45:10 +0800

    Add files via upload

---
+++ JunGuo_ICIP_2015_code/ReadMe.m
@@ -0,0 +1,29 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Descripion: 
+% This source code is for our paper
+% Jun Guo, Yanqing Guo, Yi Li, Bo Wang, and Ming Li, 
+% "Locality Sensitive Discriminative Dictionary Learning," In ICIP 2015.
+%   -Source code version 1.0  2016/03/01 by Jun Guo
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Note 1: 
+% The features (Extended Yale B dataset) used in this code is from 
+% Dr. Jiang: http://www.umiacs.umd.edu/~zhuolin/projectlcksvd.html.
+% We have already split the original data for this dataset. 
+% For other datasets, we recommend to pre-process data via 'normcols.m'.
+% Note 2:
+% For experiments on AR and Caltech 101 datasets, 
+% we also used the features provided by Dr. Jiang.
+% Please refer to our paper for detailed experimental settings.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Please cite our work if you find the code helpful.
+% @inproceedings{JunGuo_ICIP_2015,
+%   author = {J. Guo and Y. Guo and Y. Li and B. Wang and M. Li},
+%   title = {Locality Sensitive Discriminative Dictionary Learning},
+%   booktitle = {Proc. IEEE Int. Conf. Image Process. (ICIP)},
+%   address = {Quebec City, Canada},
+%   pages = {1558-1562},
+%   month = {Sep.},
+%   year = {2015}
+% }
+% If you have problems, please contact us at eeguojun@outlook.com
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
